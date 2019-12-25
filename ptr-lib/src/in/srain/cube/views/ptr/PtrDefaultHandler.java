3f96be0cff5970f797c00833851e1906a2b5c091
zhangzhao <543135445@qq.com>
2019-12-25 18:57:45 +0800

    添加recyclerView刷新支持

--- ptr-lib/src/in/srain/cube/views/ptr/PtrDefaultHandler.java
+++ ptr-lib/src/in/srain/cube/views/ptr/PtrDefaultHandler.java
@@ -1,23 +1,38 @@
 package in.srain.cube.views.ptr;
 
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AbsListView;
-import android.widget.ScrollView;
 
 public abstract class PtrDefaultHandler implements PtrHandler {
 
     public static boolean canChildScrollUp(View view) {
-        if (android.os.Build.VERSION.SDK_INT < 14) {
-            if (view instanceof AbsListView) {
-                final AbsListView absListView = (AbsListView) view;
-                return absListView.getChildCount() > 0
-                        && (absListView.getFirstVisiblePosition() > 0 || absListView.getChildAt(0)
-                        .getTop() < absListView.getPaddingTop());
+        if (view instanceof AbsListView) {
+            final AbsListView absListView = (AbsListView) view;
+            return absListView.getChildCount() > 0
+                    && (absListView.getFirstVisiblePosition() > 0 || absListView.getChildAt(0)
+                    .getTop() < absListView.getPaddingTop());
+        } else if (view instanceof RecyclerView) {
+            RecyclerView.LayoutManager manager = ((RecyclerView) view).getLayoutManager();
+            if (manager.getChildCount() == 0) return false;
+            if (manager.canScrollVertically()) {
+                View firstVisibleView = manager.getChildAt(0);
+                if (firstVisibleView == null) return false;
+                int firstVisibleViewPosition = manager.getPosition(firstVisibleView);
+                int maxTopPadding = 0;
+                for (int i = 0; i < ((RecyclerView) view).getItemDecorationCount(); i++) {
+                    RecyclerView.ItemDecoration itemDecoration = ((RecyclerView) view).getItemDecorationAt(i);
+                    Rect outRect = new Rect();
+                    itemDecoration.getItemOffsets(outRect, firstVisibleViewPosition, (RecyclerView) view);
+                    maxTopPadding = Math.max(maxTopPadding, outRect.top);
+                }
+                return firstVisibleViewPosition > 0 || firstVisibleView.getTop() + maxTopPadding < view.getPaddingTop();
             } else {
-                return view.getScrollY() > 0;
+                return true;
             }
         } else {
-            return view.canScrollVertically(-1);
+            return view.getScrollY() > 0;
         }
     }
 
--- ptr-lib/src/in/srain/cube/views/ptr/PtrDefaultHandler2.java
+++ ptr-lib/src/in/srain/cube/views/ptr/PtrDefaultHandler2.java
@@ -1,5 +1,7 @@
 package in.srain.cube.views.ptr;
 
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ScrollView;
@@ -7,24 +9,38 @@ import android.widget.ScrollView;
 public abstract class PtrDefaultHandler2 extends PtrDefaultHandler implements PtrHandler2 {
 
     public static boolean canChildScrollDown(View view) {
-        if (android.os.Build.VERSION.SDK_INT < 14) {
-            if (view instanceof AbsListView) {
-                final AbsListView absListView = (AbsListView) view;
-                return absListView.getChildCount() > 0
-                        && (absListView.getLastVisiblePosition() < absListView.getChildCount() - 1
-                        || absListView.getChildAt(absListView.getChildCount() - 1).getBottom() > absListView.getPaddingBottom());
-            } else if (view instanceof ScrollView) {
-                ScrollView scrollView = (ScrollView) view;
-                if (scrollView.getChildCount() == 0) {
-                    return false;
-                } else {
-                    return scrollView.getScrollY() < scrollView.getChildAt(0).getHeight() - scrollView.getHeight();
-                }
-            } else {
+        if (view instanceof AbsListView) {
+            final AbsListView absListView = (AbsListView) view;
+            return absListView.getChildCount() > 0
+                    && (absListView.getLastVisiblePosition() < absListView.getChildCount() - 1
+                    || absListView.getChildAt(absListView.getChildCount() - 1).getBottom() > absListView.getPaddingBottom());
+        } else if (view instanceof ScrollView) {
+            ScrollView scrollView = (ScrollView) view;
+            if (scrollView.getChildCount() == 0) {
                 return false;
+            } else {
+                return scrollView.getScrollY() < scrollView.getChildAt(0).getHeight() - scrollView.getHeight();
+            }
+        } else if (view instanceof RecyclerView) {
+            RecyclerView.LayoutManager manager = ((RecyclerView) view).getLayoutManager();
+            if (manager.getChildCount() == 0) return false;
+            if (manager.canScrollVertically()) {
+                View lastVisibleView = manager.getChildAt(((RecyclerView) view).getChildCount() - 1);
+                if (lastVisibleView == null) return false;
+                int lastVisibleViewPosition = manager.getPosition(lastVisibleView);
+                int maxBottomPadding = 0;
+                for (int i = 0; i < ((RecyclerView) view).getItemDecorationCount(); i++) {
+                    RecyclerView.ItemDecoration itemDecoration = ((RecyclerView) view).getItemDecorationAt(i);
+                    Rect outRect = new Rect();
+                    itemDecoration.getItemOffsets(outRect, lastVisibleViewPosition, (RecyclerView) view);
+                    maxBottomPadding = Math.max(maxBottomPadding, outRect.bottom);
+                }
+                return lastVisibleViewPosition < manager.getChildCount() - 1 || lastVisibleView.getBottom() + maxBottomPadding > view.getPaddingBottom();
+            } else {
+                return true;
             }
         } else {
-            return view.canScrollVertically(1);
+            return false;
         }
     }
 
